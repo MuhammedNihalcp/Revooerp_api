5833c09c417fbea8aa7c1fd3db452d965b6a7828
MuhammedNihalcp <nihalriju915@gmail.com>
2023-03-29 17:00:14 +0530

    add url file

---
+++ lib/commen/api_url.dart
@@ -0,0 +1,5 @@
+class ApiUrl {
+  final loginUrl = 'https://api.revooerp.com/auth/login';
+  final dataUrl =
+      'https://api.revooerp.com/ledger/account/a5a87c62-ad21-11ed-8b93-06cfc4b99409/';
+}
